d3395e69d2f838afe8f664edcfc3589fca4bdc82
Manav patel <manavrpatel2111@gmail.com>
2023-12-20 09:28:39 +0530

    added users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.username
+    
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+        }
